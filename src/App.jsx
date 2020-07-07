acea38f29d1e74b04cdb35587e6335f7eb91362b
Kirill Tkachyov <k2.tkachyov@itransition.com>
2020-07-07 18:17:57 +0300

    feat(App): integrate-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button, Icon} from 'mortgage-ui-kit';
+
 import './App.css';
+import 'mortgage-ui-kit/dist/main.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button title="asjdasd"/>
+        <Icon name="login" size="huge"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
